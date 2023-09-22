3a949783928bf45d539d6889bf1f5d30beebfe38
ivan.pronin <ivan.pronin@felitz.com>
2023-09-22 17:53:02 +0300

    intrinsic value for next quarter is ready

--- predicter.py
+++ predicter.py
@@ -34,11 +34,11 @@ def intrinsic_value(df):
             df.loc[row, 'CDCF'] = df.loc[row+1, 'CDCF']+df.loc[row, 'DCF']
     df['Market_value'] = (df['FCF']/(df['Discount_rate']/4-df['Grow_rate']))*df['Discount_factor']+df['CDCF']
     df['Intrinsic Value'] = df['Market_value']/df['Shares Outstanding']
-    # q1 = np.percentile(df['Intrinsic Value'], 25)
-    # q3 = np.percentile(df['Intrinsic Value'], 75)
-    # iqr = q3 - q1
-    # lower_bound = q1 - 1.5 * iqr
-    # upper_bound = q3 + 1.5 * iqr
-    # df.loc[(df['Intrinsic Value'] > upper_bound) | (df['Intrinsic Value'] < lower_bound), 'Intrinsic Value'] = np.nan
-    # df['Intrinsic Value'].interpolate(method='nearest', inplace=True)
+    q1 = np.percentile(df['Intrinsic Value'], 25)
+    q3 = np.percentile(df['Intrinsic Value'], 75)
+    iqr = q3 - q1
+    lower_bound = q1 - 2 * iqr
+    upper_bound = q3 + 2 * iqr
+    df.loc[(df['Intrinsic Value'] > upper_bound) | (df['Intrinsic Value'] < lower_bound), 'Intrinsic Value'] = np.nan
+    df['Intrinsic Value'].interpolate(method='nearest', inplace=True)
     return df
