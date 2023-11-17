eabb9568b95f0ec60ba8ba09079e1b305de406c8
ivan.pronin <ivan.pronin@felitz.com>
2023-11-17 16:56:46 +0300

    add sleep time to updater for restore memory and IP for target_web_site

--- mysql_updater_docker.py
+++ mysql_updater_docker.py
@@ -1,5 +1,6 @@
 import re
 import json
+import time
 import os
 from gc import collect
 from datetime import datetime
@@ -20,16 +21,19 @@ os.system('mkdir problems_db_updater')
 current_date = datetime.now().date()
 
 # create engine drivers for mysql
-engine = create_engine(
-    "mysql://#############")
-engine2 = create_engine(
-    "mysql://#############")
 
-# load dict for companies
+engine = create_engine(
+    "mysql://############")
+engine2 = create_engine(
+    "mysql://############")
+
+# load dict for stocks
+
 with open('stock_dict.json', 'r') as f:
     company_names = json.load(f)
 
 # intializing chromedriver
+
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
@@ -37,6 +41,7 @@ options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
 # load latest interest_rate.csv statistics
+
 interest_rate = pd.read_csv(
     'https://fred.stlouisfed.org/graph/fredgraph.csv?id=FEDFUNDS')
 interest_rate = interest_rate.set_axis(['Date', 'Discount_rate'], axis=1)
@@ -48,6 +53,7 @@ interest_rate['Date'] = pd.to_datetime(interest_rate['Date'])
 print('Table interest_rate.csv was successfully downloaded and transform')
 
 # load sp500 statistics
+
 sp500 = pd.read_csv('sp500history.csv')
 sp500.drop(columns=['Open', 'High', 'Low', 'Vol.', 'Change %'], inplace=True)
 sp500['Date'] = pd.to_datetime(sp500['Date'])
@@ -58,6 +64,7 @@ print('Table sp500.csv was successfully downloaded and transform')
 
 
 # functions: to get company name from dict
+
 def get_company_name(entered_ticket):
     """return company name from dict"""
     company_name = company_names[entered_ticket]
@@ -65,10 +72,13 @@ def get_company_name(entered_ticket):
 
 
 # re pattern compile, script
+
 pattern = re.compile(r'var originalData = (\[.*?\]);', re.DOTALL)
 script = 'return document.querySelector("body > div.main_content_container.container-fluid > script:nth-child(7)").textContent;'
 
+
 # functions for scrapping
+
 def get_df_from_url(url, webdriver_):
     """return DataFrame from url
     :type arg1: str
@@ -76,6 +86,7 @@ def get_df_from_url(url, webdriver_):
     :return: dataframe
     :rtype: pd.DataFrame
     """
+
     webdriver_.get(url)
     data_raw = webdriver_.execute_script(script)
     data = pattern.search(data_raw).group(1)  # type: ignore
@@ -87,8 +98,11 @@ def get_df_from_url(url, webdriver_):
     return df
 
 # functions for scrapping
+
+
 def df_for_msql(entered_ticket, webdriver_):
     """ Return DataFrame from URLs, concatenating three DataFrame in one"""
+
     company_name = get_company_name(entered_ticket)
     urls = {
         'url1': f'https://www.macrotrends.net/stocks/charts/{entered_ticket}/{company_name}/balance-sheet?freq=Q',
@@ -96,7 +110,7 @@ def df_for_msql(entered_ticket, webdriver_):
         'url3': f'https://www.macrotrends.net/stocks/charts/{entered_ticket}/{company_name}/cash-flow-statement?freq=Q'
     }
     df_concat = pd.DataFrame()
-    for url, values in urls.items():
+    for _, values in urls.items():
         df = get_df_from_url(values, webdriver_)
         df_concat = pd.concat([df_concat, df], axis=0)
     df_concat = df_concat.transpose().reset_index()
@@ -126,49 +140,90 @@ def df_for_msql(entered_ticket, webdriver_):
     return df
 
 
+def yfinance_updater(ticket):
+    """yfinance_updater"""
+
+    data_ticket = yf.Ticker(ticket)
+    df = data_ticket.history(period="max", interval='1mo').reset_index()
+    df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.sort_values(
+        by='Date', ascending=False).reset_index()
+    df.drop(columns=['index'], inplace=True)
+
+    return df
+
+
 print('All functions, dicts and drivers were succesfully launched')
 
 # Updating for SP500 Database
-problems_with_scraping = []
-for ticket in company_names:
-    try:
-        df_new = df_for_msql(ticket, driver)
-        df_new.to_sql(name=f'{ticket}', con=engine,
-                      if_exists='replace', index=False)
-        print(f'load of {ticket} to SP500 was successful')
-    except Exception as e:
-        problems_with_scraping.append(ticket)
-        print(f'load of {ticket} failed with error: {e}')
-    collect()
 
-joblib.dump(problems_with_scraping, f'./problems_db_updater/scrapping_problems_{current_date}.txt')
+problems_with_scraping = []
+
+for ticket in company_names:
+    success = False
+    while not success:
+        try:
+            df_new = df_for_msql(ticket, driver)
+            df_new.to_sql(name=f'{ticket}', con=engine,
+                          if_exists='replace', index=False)
+            print(f'load of {ticket} to SP500 was successful')
+            collect()
+            success = True
+        except Exception:
+            time.sleep(600)  # Wait for 10 minutes before retrying
+            collect()
+            try:
+                df_new = df_for_msql(ticket, driver)
+                df_new.to_sql(name=f'{ticket}', con=engine,
+                              if_exists='replace', index=False)
+                print(f'load of {ticket} to SP500 was successful')
+                collect()
+                success = True
+            except Exception as e:
+                problems_with_scraping.append(ticket)
+                print(f'load of {ticket} failed with error: {e}')
+                collect()
+joblib.dump(problems_with_scraping,
+            f'./problems_db_updater/scrapping_problems_{current_date}.txt')
 
 print('Updating for SP500 Databasee was finished succesfully')
 
-# Updating for YFINANCE Datebase
-problems_with_yfinance = []
-for ticket in company_names:
-    try:
-        data_stock = yf.Ticker(ticket)
-        stock_df = data_stock.history(
-            period="max", interval='1mo').reset_index()
-        stock_df['Date'] = pd.to_datetime(stock_df['Date'], errors='coerce')
-        stock_df['Date'] = stock_df['Date'].dt.strftime('%Y-%m-%d')
-        stock_df['Date'] = pd.to_datetime(stock_df['Date'])
-        stock_df = stock_df.sort_values(
-            by='Date', ascending=False).reset_index()
-        stock_df.drop(columns=['index'], inplace=True)
-        stock_df.to_sql(name=f'{ticket}', con=engine2,
-                        if_exists='replace', index=False)
-        print(f'load of {ticket} to YFINANCE was successfull')
-    except Exception as e:
-        problems_with_yfinance.append(ticket)
-        print(f'load of {ticket} failed with error: {e}')
-    collect()
 
-joblib.dump(problems_with_yfinance, f'./problems_db_updater/yfinance_problems_{current_date}.txt')
+# Updating for YFINANCE Datebase
+
+problems_with_yfinance = []
+
+for ticket in company_names:
+    success = False
+    while not success:
+        try:
+            stock_df = yfinance_updater(ticket)
+            stock_df.to_sql(name=f'{ticket}', con=engine2,
+                            if_exists='replace', index=False)
+            print(f'load of {ticket} to YFINANCE was successfull')
+            collect()
+            success = True
+        except Exception:
+            time.sleep(600)  # Wait for 10 minutes before retrying
+            collect()
+            try:
+                stock_df = yfinance_updater(ticket)
+                stock_df.to_sql(name=f'{ticket}', con=engine2,
+                                if_exists='replace', index=False)
+                print(f'load of {ticket} to YFINANCE was successfull')
+                collect()
+                success = True
+            except Exception as e:
+                problems_with_yfinance.append(ticket)
+                print(f'load of {ticket} failed with error: {e}')
+                collect()
+joblib.dump(problems_with_yfinance,
+            f'./problems_db_updater/yfinance_problems_{current_date}.txt')
 
 print('Updating for YFINANCE Datebase was finished succesfully')
+
 
 # Creating TOP10 table for YFINANCE Datebase
 problems_with_top10 = []
@@ -179,7 +234,7 @@ for ticket in company_names:
         df_iv_price = intrinsic_value_curr(
             pd.read_sql_table(ticket, engine), data_stock)
         data_stock = data_stock.loc[data_stock['Date']
-                                            <= df_iv_price.loc[0, 'Date']].reset_index()
+                                    <= df_iv_price.loc[0, 'Date']].reset_index()
         last_close_price = data_stock.loc[0, 'Close']
         last_iv_value = df_iv_price.loc[0, 'Intrinsic Value']
         diff_price = df_iv_price.loc[0, 'Intrinsic Value'].astype(  # type: ignore
@@ -189,20 +244,20 @@ for ticket in company_names:
                                              'Difference': diff_price}, orient='index').transpose()
         df_ = pd.concat([df_, df_compare], axis=0)
         print(f'Add ticket {ticket} while creating top10 table was succesfull')
+        collect()
     except Exception as e:
         problems_with_top10.append(ticket)
         print(
             f'Add ticket {ticket} while creating top10 failed with error {e}')
-    collect()
 
 top10 = df_.sort_values(by='Difference', ascending=False).head(10)
 top10['symbol'].apply(lambda x: f'{x}: {company_names[x]}')
 top10.to_sql(name='TOP10', con=engine, if_exists='replace', index=False)
 
-joblib.dump(problems_with_top10, f'./problems_db_updater/top10_problems_{current_date}.txt')
+joblib.dump(problems_with_top10,
+            f'./problems_db_updater/top10_problems_{current_date}.txt')
 
 print('Proccess of downloading and uploadeding for top10 table to DataBase successfully finished')
-
 
 
 print('All updates were fineshed')
